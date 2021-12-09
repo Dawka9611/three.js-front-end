d0d2ee4ac970c32cf8cad17512dcd757f3c1322f
Dawka9611 <udawaadulam9611@gmail.com>
2021-12-09 17:46:42 +0800

    kekkeek

--- src/components/page1/page1.js
+++ src/components/page1/page1.js
@@ -1,7 +1,8 @@
-import { useContext, useEffect, useState } from 'react'
-import { Link, useLocation, Prompt } from 'react-router-dom'
+import { useContext, useState } from 'react'
+import { Link, Prompt } from 'react-router-dom'
 import AlertContext from '../../contexts/alert.context'
 import styles from './page1.module.css'
+import { BsExclamation } from 'react-icons/bs'
 
 const initialState = {
    name: null,
@@ -26,7 +27,7 @@ export const tableRow = {
 }
 
 export default function Page1() {
-   const { alert, setAlert } = useContext(AlertContext)
+   const { setAlert } = useContext(AlertContext)
 
    const [state, setState] = useState(initialState)
    const [state1, setState1] = useState(initialState1)
@@ -34,17 +35,25 @@ export default function Page1() {
 
    const [editted, setEditted] = useState(false)
    const [submitted, setSubmitted] = useState(false)
-   const [filled, setUFilled] = useState(true)
+   const [validate, setValidate] = useState(false)
+
+   const validSet = new Set()
+   console.log(validSet)
 
    function setter(key, value) {
       setState(prev => ({ ...prev, [key]: value }))
       setEditted(true)
-      value.length > 0 ? setUFilled(false) : setUFilled(true)
+      validSet.add(state.name)
+      validSet.add(state.age)
+      console.log(validSet)
    }
 
    function setter1(key, value) {
       setState1(prev => ({ ...prev, [key]: value }))
-      value.length > 0 ? setUFilled(false) : setUFilled(true)
+      setEditted(true)
+      validSet.add(state1.gender)
+      validSet.add(state1.height)
+      console.log(validSet)
    }
 
    function setterTable(key, value, index) {
@@ -53,13 +62,19 @@ export default function Page1() {
          next[index][key] = value
          return next
       })
-      value.length > 0 ? setUFilled(false) : setUFilled(true)
+      setEditted(true)
    }
 
    function submit() {
-      setSubmitted(true)
-      if (submitted && !filled) {
-         window.alert('submit')
+      setValidate(true)
+
+      let valid = true
+      valid = valid && !Object.values(state).some(value => [null, undefined, ''].includes(value))
+      valid = valid && !Object.values(state1).some(value => [null, undefined, ''].includes(value))
+
+      if (valid) {
+         window.alert('submitted')
+         setSubmitted(true)
          setEditted(false)
       } else {
          setAlert({
@@ -79,21 +94,11 @@ export default function Page1() {
             <div className="">
                Submitted: {submitted + ''}
             </div>
-            <Input value={state.name} setter={setter} name='name' />
-            <input value={state.age ?? ''}
-               onChange={e => setter('age', e.target.value)}
-               className={`${styles.input} ${filled && submitted ? styles.inputfilled : ''}`}
-            />
-            <input value={state1.gender ?? ''}
-               onChange={e => setter1('gender', e.target.value)}
-               className={`${styles.input} ${filled && submitted ? styles.inputfilled : ''}`}
-            />
-            <input
-               value={state1.height ?? ''}
-               onChange={e => setter1('height', e.target.value)}
-               className={`${styles.input} ${filled && submitted ? styles.inputfilled : ''}`}
-            />
-            <table>
+            <Input value={state.name} setter={setter} name='name' validate={validate} />
+            <Input value={state.age} setter={setter} name='age' validate={validate} />
+            <Input value={state1.gender} setter={setter1} name='gender' validate={validate} />
+            <Input value={state1.height} setter={setter1} name='height' validate={validate} />
+            <table className={styles.table}>
                <thead>
                   <tr>
                      {receivablesReportHeaders.map(headers =>
@@ -107,29 +112,16 @@ export default function Page1() {
                   {table.map((row, i) =>
                      <tr key={i}>
                         <td>
-                           <input
-                              value={row.buyerName}
-                              onChange={e => setterTable('buyerName', e.target.value, i)}
-                              className={`${styles.input} ${filled && submitted ? styles.inputfilled : ''}`}
-                           />
+                           <Input value={row.buyerName} setter={setterTable} name='buyerName' index={i} />
                         </td>
                         <td>
-                           <input
-                              value={row.maxLoanTerm}
-                              onChange={e => setterTable('maxLoanTerm', e.target.value, i)}
-                              className={`${styles.input} ${filled && submitted ? styles.inputfilled : ''}`}
-                           />
+                           <Input value={row.maxLoanTerm} setter={setterTable} name='maxLoanTerm' index={i} />
                         </td>
                         <td>
-                           <input
-                              value={row.currentCondition}
-                              onChange={e => setterTable('currentCondition', e.target.value, i)}
-                              className={`${styles.input} ${filled && submitted ? styles.inputfilled : ''}`}
-                           />
+                           <Input value={row.currentCondition} setter={setterTable} name='currentCondition' index={i} />
                         </td>
                      </tr>
                   )}
-
                </tbody>
             </table>
             <div className={styles.buttonConatiner}>
@@ -149,14 +141,20 @@ export default function Page1() {
    )
 }
 
-function Input(value, name, index, setter) {
+export function Input({ value, name, index, setter, style, validate,
+   invalidValues = [null, undefined, '']
+}) {
    return (
       <div>
-         <input
-            value={value ?? ''}
-            onChange={e => setter(name, e.target.value, index)}
-            className={styles.input}
-         />
+         <div className={styles.inputConatiner}>
+            {validate && invalidValues.includes(value) && <BsExclamation className={styles.icon} />}
+            <input
+               value={value ?? ''}
+               onChange={e => setter(name, e.target.value, index)}
+               className={validate && invalidValues.includes(value) ? styles.inputUnfilled : ''}
+            />
+         </div>
+
       </div>
    )
 }
